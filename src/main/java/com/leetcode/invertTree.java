cf9bbb3177111494c08da326992a8b9f6cb25f4f
liuyu5 <liuyu5@liepin.com>
2020-08-10 13:57:13 +0800

    some new code

---
+++ src/main/java/com/leetcode/invertTree.java
@@ -0,0 +1,17 @@
+package com.leetcode;
+
+import com.leetcode.lib.TreeNode;
+
+public class invertTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root ==null){
+            return null;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
---
+++ src/main/java/com/leetcode/middleNode.java
@@ -0,0 +1,15 @@
+package com.leetcode;
+
+import com.leetcode.lib.ListNode;
+
+public class middleNode {
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
